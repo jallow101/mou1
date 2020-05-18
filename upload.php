575968a5e0f3b69b48a9689a337fe03190985971
jallow101 <jallow101@users.noreply.github.com>
2020-05-18 11:44:09 +0000

    Update request.php

--- upload.php
+++ upload.php
@@ -6,23 +6,24 @@
       $file_tmp = $_FILES['report']['tmp_name'];
       $file_type = $_FILES['report']['type'];
       $file_ext=strtolower(end(explode('.',$_FILES['report']['name'])));
-      
+
       $extensions= array("pdf","doc","docx");
-      
+
       if(in_array($file_ext,$extensions)=== false){
          $errors[]="extension not allowed, please choose a DOC or PDF file.";
-         
+
       }
-      
+
       if($file_size > 500000000) {
          $errors[]='File size must be excately 500 MB';
       }
-      
+
       if(empty($errors)==true) {
          move_uploaded_file($file_tmp,"uploads/".$file_name);
          echo "Success";
       }else{
          print_r($errors);
+         $Test = 2;
       }
    }
 ?>
